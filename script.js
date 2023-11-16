1433ae41541570df93029aa0148a89d6244baf3a
redeyescripts <pals.sten@gmail.com>
2023-11-16 20:15:41 +0200

    under dev

---
+++ script.js
@@ -0,0 +1,57 @@
+function toggler(divId) {
+    $("#" + divId).toggle();
+}
+$(".search-menu").hide(); 
+/*function f1() {
+    toggler('search-menu');
+    $(".search-menu").show();
+    
+
+}*/
+
+function divshower() {
+    var x = document.getElementById("search-menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+
+// TESTI FUNKTSIOON MIS KUVAB ETTE OTSINGU REALT SISESTATUD TEKSTI ELEMENTI
+function done() {
+    var x = document.getElementById("bar").value;
+    var z = document.querySelector(".inf-out").textContent = x;
+    const t = "OTSINGU RIBA ON TÜHI!"
+    if (x == "") {
+        document.querySelector(".inf-out").textContent = "PALUN SISESTAGE PÄRINGUD";
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("id", "error");
+        newDiv.innerHTML = t;
+        newDiv.style.backgroundColor = "red";
+        newDiv.style.color = "white";
+        newDiv.style.padding = "10px";
+        newDiv.style.marginLeft = "150px"
+        newDiv.style.marginTop = "209px"
+        newDiv.style.fontSize = "20px"
+    } else {
+        const element = document.getElementById("error");
+        element.remove(); 
+    }
+    
+
+    document.querySelector(".inf-out").textContent = z;
+   
+    const parent = document.getElementById("search-menu");
+    parent.appendChild(newDiv);
+}
+
+
+
+
+
+
+
+
+  
